341286c6ce14509886cadbb205443281958e4c24
JackatDJL <71508487+JackatDJL@users.noreply.github.com>
2025-03-28 21:14:25 +0100

    Finalised Neverthrow

--- src/server/api/routers/wahlen.ts
+++ src/server/api/routers/wahlen.ts
@@ -21,53 +21,18 @@ const createWahlType = z.object({
   owner: z.string().length(32),
 });
 
-enum createWahlErrorTypes {
+enum wahlErrorTypes {
   Failed = "Failed",
   Disallowed = "Disallowed",
+  NotFound = "NotFound",
 }
 
-type createWahlError = {
-  type: createWahlErrorTypes;
+type wahlError = {
+  type: wahlErrorTypes;
   message: string;
 };
 
-export const wahlenRouter = createTRPCRouter({
-  create: protectedProcedure
-    .input(createWahlType)
-    .mutation(
-      async ({
-        input,
-      }): Promise<Result<typeof wahlen.$inferSelect, createWahlError>> => {
-        const insertable: typeof wahlen.$inferInsert = {
-          id: randomUUID(),
-          shortname: input.shortname,
-
-          title: input.title,
-          description: input.description,
-
-          owner: input.owner,
-        };
-        const { data: insertableResponse, error: insertableError } = await tc(
-          db.insert(wahlen).values(insertable).returning(),
-        );
-        if (insertableError) {
-          return err({
-            type: createWahlErrorTypes.Failed,
-            message: insertableError.message,
-          });
-        }
-
-        const response = insertableResponse[0] ? insertableResponse[0] : null;
-        if (!response) {
-          throw new Error("Failed to create wahl");
-        }
-
-        return response[0];
-      },
-    ),
-  edit: protectedProcedure
-    .input(
-      z.object({
+const editWahlType = z.object({
         id: z.string().uuid(),
         shortname: z.string().min(3).max(25).optional(),
         status: z
@@ -95,49 +60,116 @@ export const wahlenRouter = createTRPCRouter({
         archiveDate: z.date().optional(),
 
         updatedAt: z.date().optional(),
-      }),
-    )
-    .mutation(async ({ input }) => {
-      const data = await db
-        .select()
-        .from(wahlen)
-        .where(eq(wahlen.id, input.id));
+      })
 
-      if (!data[0]) {
-        throw new Error("Wahl not found");
+      
+
+export const wahlenRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(createWahlType)
+    .mutation(
+      async ({
+        input,
+      }): Promise<Result<typeof wahlen.$inferSelect, wahlError>> => {
+        const insertable: typeof wahlen.$inferInsert = {
+          id: randomUUID(),
+          shortname: input.shortname,
+
+          title: input.title,
+          description: input.description,
+
+          owner: input.owner,
+        };
+        const { data: insertableResponse, error: insertableError } = await tc(
+          db.insert(wahlen).values(insertable).returning(),
+        );
+        if (insertableError) {
+          return err({
+            type: wahlErrorTypes.Failed,
+            message: insertableError.message,
+          });
+        }
+
+        const response = insertableResponse[0] ? insertableResponse[0] : null;
+        if (!response) {
+          throw new Error("Failed to create wahl");
+        }
+
+        return ok(response);
+      },
+    ),
+  edit: protectedProcedure
+    .input(
+      editWahlType
+    )
+    .mutation(async ({ input }): Promise<Result<typeof wahlen.$inferSelect, wahlError>> => {
+
+      const {data: dataArray, error: dbError} = await tc(
+        db.select().from(wahlen).where(eq(wahlen.id, input.id)));
+      if (dbError) {
+        return err({
+          type: wahlErrorTypes.Failed,
+          message: dbError.message,
+        });
       }
+
+        const data = dataArray[0] ? dataArray[0] : null;
+        if (!data) {
+          return err({
+            type: wahlErrorTypes.Failed,
+            message: "Wahl not found",
+          });
+        }
 
       const insertable: typeof wahlen.$inferInsert = {
         id: input.id,
-        shortname: input.shortname ?? data[0].shortname,
-        status: input.status ?? data[0].status,
+        shortname: input.shortname ?? data.shortname,
+        status: input.status ?? data.status,
 
-        alert: input.alert ?? data[0].alert,
-        alertMessage: input.alertMessage ?? data[0].alertMessage,
+        alert: input.alert ?? data.alert,
+        alertMessage: input.alertMessage ?? data.alertMessage,
 
-        title: input.title ?? data[0].title,
-        description: input.description ?? data[0].description,
+        title: input.title ?? data.title,
+        description: input.description ?? data.description,
 
-        owner: input.owner ?? data[0].owner,
+        owner: input.owner ?? data.owner,
 
-        startDate: input.startDate ?? data[0].startDate,
-        endDate: input.endDate ?? data[0].endDate,
-        archiveDate: input.archiveDate ?? data[0].archiveDate,
+        startDate: input.startDate ?? data.startDate,
+        endDate: input.endDate ?? data.endDate,
+        archiveDate: input.archiveDate ?? data.archiveDate,
 
-        createdAt: data[0].createdAt,
-        updatedAt: input.updatedAt ?? data[0].updatedAt,
+        createdAt: data.createdAt,
+        updatedAt: input.updatedAt ?? data.updatedAt,
       };
 
-      const response = await db
-        .update(wahlen)
-        .set(insertable)
-        .where(eq(wahlen.id, input.id))
-        .returning();
-      if (!response[0]) {
-        throw new Error("Failed to update wahl");
+      // const response = await db
+      //   .update(wahlen)
+      //   .set(insertable)
+      //   .where(eq(wahlen.id, input.id))
+      //   .returning();
+      // if (!response[0]) {
+      //   throw new Error("Failed to update wahl");
+      // }
+
+      const { data: insertableResponse, error: insertableError } = await tc(
+        db.update(wahlen).set(insertable).where(eq(wahlen.id, input.id)).returning(),
+      );
+      if (insertableError) {
+        return err({
+          type: wahlErrorTypes.Failed,
+          message: insertableError.message,
+        });
       }
 
-      return response[0];
+      const response = insertableResponse[0] ? insertableResponse[0] : null;
+      if (!response) {
+        return err({
+          type: wahlErrorTypes.Failed,
+          message: "Failed to update wahl",
+        });
+      }
+
+      return ok(response)
     }),
   getByShortname: publicProcedure.query(() => ""),
 });
