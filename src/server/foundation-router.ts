e13d94d1893c107b8235731585190c0ffb750a27
JackatDJL <71508487+JackatDJL@users.noreply.github.com>
2025-04-01 20:18:54 +0200

    Add API route for printout and implement header and path retrieval; introduce forbidden shortnames list

---
+++ src/server/foundation-router.ts
@@ -0,0 +1,90 @@
+"use server";
+
+import { randomUUID } from "crypto";
+import { redirect } from "next/navigation";
+import { cookies, headers } from "next/headers";
+import { devModeFlag, shortnameDevInterface } from "~/server/flags";
+
+export async function cookieOverwrite() {
+  return (await shortnameDevInterface()) === "@ovr/cookie";
+}
+
+async function shortnameOverwrite() {
+  const res = await shortnameDevInterface();
+  switch (res) {
+    case "@ovr/cookie":
+      const cook = await cookies();
+      return cook.get("x-short-overwrite")?.value ?? randomUUID();
+    case "@ovr/not-found":
+      return randomUUID();
+    default:
+      return res;
+  }
+}
+
+export async function getHeaders() {
+  const headerList = await headers();
+  const headersObject: Record<string, string> = {};
+
+  headerList.forEach((value: string, key: string) => {
+    headersObject[key] = value;
+  });
+
+  return headersObject;
+}
+
+export async function getCurrentPath() {
+  const headersObject = await getHeaders();
+  const url = headersObject["x-clerk-clerk-url"];
+  const currentPath = url ? new URL(url).pathname : "/";
+  return currentPath;
+}
+
+const rootDomain = "wahlen.djl.foundation";
+const catchDomain = "wahl.djl.foundation";
+
+const catchPathsRegex = new RegExp(`^(\/|\/wahl\/.*)$`);
+
+export async function handleRouting(
+  targetPath: string,
+  string: boolean = false,
+) {
+  const headers = await getHeaders();
+  const path = await getCurrentPath();
+  const dev = await devModeFlag();
+  const shortname = await shortnameOverwrite();
+
+  if (dev || shortname) {
+    if (string) return targetPath;
+    redirect(targetPath);
+  } else {
+    if (headers.host !== rootDomain) {
+      if (targetPath !== catchPathsRegex.source) {
+        if (string) return targetPath;
+        redirect(rootDomain + targetPath);
+      } else {
+        if (string) return targetPath;
+        redirect(headers.host + targetPath);
+      }
+    } else {
+      if (string) return targetPath;
+      redirect(targetPath);
+    }
+  }
+}
+
+export async function cleanup() {
+  const headers = await getHeaders();
+  const path = await getCurrentPath();
+  const dev = await devModeFlag();
+  const shortname = await shortnameOverwrite();
+
+  if (catchPathsRegex.test(path)) {
+    if (dev || shortname) {
+      return;
+    } else {
+      const url = new URL(`https://${rootDomain}/${path}`);
+      redirect(url.toString());
+    }
+  }
+}
