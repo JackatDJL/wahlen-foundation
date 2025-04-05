f6eafce098e134bfff064a7b2006cc6f5654e0c3
JackatDJL <71508487+JackatDJL@users.noreply.github.com>
2025-04-05 19:37:51 +0200

    Made the DSN env as per Recomendations

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -4,9 +4,10 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+import { env } from "~/env";
 
 Sentry.init({
-  dsn: "https://3f29b412d9c0f07285edc890859afcde@o4508955313242112.ingest.de.sentry.io/4509096658993232",
+  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
